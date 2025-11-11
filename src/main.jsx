e57561453ac2f47c79dd3e607166ec7d1f337d84
vanesaromeo <vaneroeo09@gmail.com>
2025-11-11 20:32:05 -0300

    main-src

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from "./App.jsx";
 import { GlobalStyles } from "./styles/GlobalStyles.js";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import { store, persistor } from "./Redux/Store.js";
+import { store, persistor } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
