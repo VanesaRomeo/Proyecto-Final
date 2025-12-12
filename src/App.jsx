a35efbe7c7efa2d7dd5cd6c1fee65b0277a44581
vanesaromeo <vaneroeo09@gmail.com>
2025-12-11 21:01:49 -0300

    se cambio app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,4 @@
-// import { BrowserRouter as Router } from "react-router-dom";
-// import { AuthProvider } from "./context/AuthContext";
-// import AppRoutes from "./routes/Routes";
 
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <Router>
-//         <AppRoutes />
-//       </Router>
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
 import { BrowserRouter as Router } from "react-router-dom";
 
 
